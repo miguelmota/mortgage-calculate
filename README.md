1c69e5d24c9018046bcd5d94973e691e7385eca8
Miguel Mota <miguelmota2@gmail.com>
2015-11-21 03:31:16 -0800

    update readme

--- README.md
+++ README.md
@@ -14,12 +14,14 @@ bower install mortgage-calculate
 
 # Usage
 
+Calculate monthly payment
+
 ```javascript
 const mortgageCalculate = require('mortgage-calculate');
 
 const result = mortgageCalculate({
-  loanAmount: 10000,
-  APR: 5,
+  loanAmount: 100000,
+  APR: 4.7,
   termYears: 30
 });
 
@@ -27,9 +29,30 @@ console.log(result);
 /*
 {
   loanAmount: 10000,
-  totalCost: 19325.578428436973,
-  monthlyPayment: 53.682162301213815,
-  APR: 5,
+  totalCost: 186709.61139815467,
+  monthlyPayment: 518.6378094393185,
+  APR: 4.7,
+  termYears: 30
+}
+*/
+```
+
+Calculate max loan amount.
+
+```javascript
+const result = mortgageCalculate({
+  monthlyPayment: 500,
+  APR: 4.7,
+  termYears: 30
+});
+
+console.log(result);
+/*
+{
+  loanAmount: 96406.3920716719,
+  totalCost: 180000,
+  monthlyPayment: 500,
+  APR: 4.7,
   termYears: 30
 }
 */
--- test/mortgage-calculate.js
+++ test/mortgage-calculate.js
@@ -4,7 +4,7 @@ const mortgageCalculate = require('../mortgage-calculate');
 test('flowright', function (t) {
   'use strict';
 
-  t.plan(13);
+  t.plan(23);
 
   t.equal(mortgageCalculate(), null);
   t.equal(mortgageCalculate('foo'), null);
@@ -33,4 +33,28 @@ test('flowright', function (t) {
   t.equal(result_B.termYears, 30);
   t.equal(result_B.monthlyPayment, 53.682162301213815);
   t.equal(result_B.totalCost, 19325.578428436973);
+
+  const result_C = mortgageCalculate({
+    loanAmount: 100000,
+    APR: 4.7,
+    termYears: 30
+  });
+
+  t.equal(result_C.loanAmount, 100000);
+  t.equal(result_C.APR, 4.7);
+  t.equal(result_C.termYears, 30);
+  t.equal(result_C.monthlyPayment, 518.6378094393185);
+  t.equal(result_C.totalCost, 186709.61139815467);
+
+  const result_D = mortgageCalculate({
+    monthlyPayment: 500,
+    APR: 4.7,
+    termYears: 30
+  });
+
+  t.equal(result_D.loanAmount, 96406.3920716719);
+  t.equal(result_D.APR, 4.7);
+  t.equal(result_D.termYears, 30);
+  t.equal(result_D.monthlyPayment, 500);
+  t.equal(result_D.totalCost, 180000);
 });
